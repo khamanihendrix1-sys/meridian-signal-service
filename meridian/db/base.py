b2fa37f599a44394adc7c1d08535916d44e2ef4c
khamanihendrix1-sys <khamani.hendrix1@gmail.com>
2026-06-20 15:44:37 -0500

    Fix SQLAlchemy base typing for mypy

--- meridian/db/base.py
+++ meridian/db/base.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import DeclarativeBase
 
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+    pass
